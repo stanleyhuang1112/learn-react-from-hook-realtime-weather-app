5fe10b238817d8263cffb60a60b7acb5bce3d835
Chingte Huang <chingte0523@hokplay.com>
2022-10-27 10:47:32 +0800

    7-1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import styled from '@emotion/styled';
-import { ReactComponent as DayCloudyIcon } from './images/day-cloudy.svg';
-import { ReactComponent as AirFlowIcon } from './images/airFlow.svg';
-import { ReactComponent as RainIcon } from './images/rain.svg';
-import { ReactComponent as RefreshIcon } from './images/refresh.svg';
 import { ThemeProvider } from '@emotion/react';
-import { useState, useEffect } from 'react'
-import dayjs from 'dayjs'
+import { useState, useEffect, useCallback, useMemo } from 'react'
+import { getMoment } from './utils/helpers';
+import WeatherCard from './views/WeatherCard';
+import useWeatherAPI from './hooks/useWeatherAPI';
 
 const theme = {
   light: {
@@ -35,162 +33,36 @@ const Container = styled.div`
   justify-content: center;
 `;
 
-const WeatherCard = styled.div`
-  position: relative;
-  min-width: 360px;
-  box-shadow: ${({ theme }) => theme.boxShadow};
-  background-color: ${({ theme }) => theme.foregroundColor};
-  box-sizing: border-box;
-  padding: 30px 15px;
-`;
-
-const Location = styled.div`
-  font-size: 28px;
-  color: ${({ theme }) => theme.titleColor};
-  margin-bottom: 20px;
-`;
-
-const Description = styled.div`
-  font-size: 16px;
-  color: ${({ theme }) => theme.textColor};
-  margin-bottom: 30px;
-`;
-
-const CurrentWeather = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 30px;
-`;
-
-const Temperature = styled.div`
-  color: ${({ theme }) => theme.temperatureColor};
-  font-size: 96px;
-  font-weight: 300;
-  display: flex;
-`;
-
-const Celsius = styled.div`
-  font-weight: normal;
-  font-size: 42px;
-`;
-
-const AirFlow = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 16x;
-  font-weight: 300;
-  color: ${({ theme }) => theme.textColor};
-  margin-bottom: 20px;  
-  svg {
-    width: 25px;
-    height: auto;
-    margin-right: 30px;
-  }
-`;
-
-const Rain = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 16x;
-  font-weight: 300;
-  color: ${({ theme }) => theme.textColor};  
-  svg {
-    width: 25px;
-    height: auto;
-    margin-right: 30px;
-  }
-`;
-
-const Refresh = styled.div`
-  position: absolute;
-  right: 15px;
-  bottom: 15px;
-  font-size: 12px;
-  display: inline-flex;
-  align-items: flex-end;
-  color: ${({ theme }) => theme.textColor};
-  svg {
-    margin-left: 10px;
-    width: 15px;
-    height: 15px;
-    cursor: pointer;
-  }
-`;
-
-const DayCloudy = styled(DayCloudyIcon)`
-  flex-basis: 30%;
-`
-
 const AUTHORIZATION_KEY = 'CWB-FBFF0FFF-E3C6-4FB7-AA79-1A8ACBAC1BAD'
 const LOCATION_NAME = '臺北'
+const LOCATION_NAME_FORECAST = '臺北市';
 
 function App() {
   const [currentTheme, setCurrentTheme] = useState('light')
-  const [currentWeather, setCurrentWeather] = useState({
-    locationName: '臺北市',
-    description: '多雲時晴',
-    windSpeed: 1.1,
-    temperature: 22.9,
-    rainPossibility: 48.3,
-    observationTime: '2020-12-12 22:10:00',
+  const [currentWeather, fetchData] = useWeatherAPI({
+    authorizationKey: AUTHORIZATION_KEY,
+    locationName: LOCATION_NAME,
+    cityName: LOCATION_NAME_FORECAST,
   })
 
   useEffect(() => {
-    fetchCurrentWeather()
-  }, [])
+    fetchData()
+  }, [fetchData])
 
-  const fetchCurrentWeather = () => {
-    fetch(`https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=${AUTHORIZATION_KEY}&locationName=${LOCATION_NAME}`)
-      .then((response) => response.json())
-      .then((data) => {
-        const locationData = data.records.location[0]
-        const weatherElement = locationData.weatherElement.reduce(
-          (neededElements, item) => {
-            if (['WDSD', 'TEMP'].includes(item.elementName)) {
-              neededElements[item.elementName] = item.elementValue
-            }
-            return neededElements
-          }
-        )
-        setCurrentWeather({
-          observationTime: locationData.time.obsTime,
-          locationName: locationData.locationName,
-          temperature: weatherElement.TEMP,
-          windSpeed: weatherElement.WDSD,
-          description: '多雲時晴',
-          rainPossibility: 60,
-        })
-      });
-  }
+  const moment = useMemo(() => getMoment(LOCATION_NAME_FORECAST), [])
+
+  useEffect(() => {
+    setCurrentTheme(moment === 'day' ? 'light' : 'dark')
+  }, [moment])
 
   return (
     <ThemeProvider theme={theme[currentTheme]}>
       <Container>
-        <WeatherCard>
-          <Location>{currentWeather.locationName}</Location>
-          <Description>{currentWeather.description}</Description>
-          <CurrentWeather>
-            <Temperature>
-              {Math.round(currentWeather.temperature)} <Celsius>°C</Celsius>
-            </Temperature>
-            <DayCloudy />
-          </CurrentWeather>
-          <AirFlow>
-            <AirFlowIcon />{currentWeather.windSpeed} m/h
-          </AirFlow>
-          <Rain>
-            <RainIcon />{currentWeather.rainPossibility}%
-          </Rain>
-          <Refresh onClick={fetchCurrentWeather}>
-            最後觀測時間：
-            {new Intl.DateTimeFormat('zh-TW', {
-              hour: 'numeric',
-              minute: 'numeric',
-            }).format(new dayjs(currentWeather.observationTime))}
-            <RefreshIcon />
-          </Refresh>
-        </WeatherCard>
+        <WeatherCard
+          currentWeather={currentWeather}
+          moment={moment}
+          fetchData={fetchData}
+        />
       </Container>
     </ThemeProvider>
   );
